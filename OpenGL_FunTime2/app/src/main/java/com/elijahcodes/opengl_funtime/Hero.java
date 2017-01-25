270c8e322ea5cd503e62476229f14916cd07bcd2
elijahchen <code.elijahchen@gmail.com>
2017-01-24 23:28:32 -0500

    - Refactored loadTexture() method in Hero class

--- OpenGL_FunTime2/app/src/main/java/com/elijahcodes/opengl_funtime/Hero.java
+++ OpenGL_FunTime2/app/src/main/java/com/elijahcodes/opengl_funtime/Hero.java
@@ -1,8 +1,108 @@
 package com.elijahcodes.opengl_funtime;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.content.ContentValues.TAG;
+
 /**
  * Created by elijah on 1/24/2017.
  */
 
 public class Hero {
+
+    static float squareCoords[] = {
+            -0.25f, 0.25f, 0.0f, // top left
+            -0.25f, -0.25f, 0.0f, // bottom left
+            0.25f, -0.25f, 0.0f, // bottom right
+            0.25f, 0.25f, 0.0f //top right
+    };
+
+    private final short drawOrder[] = {0, 1, 2, 0, 2, 3};
+
+    private final String vertexShaderCode =
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
+                    "attribute vec2 TexCoordIn;" +
+                    "varying vec2 TexCoordOut;" +
+                    "void main() {" +
+                    " gl_position = uMVPMatrix * vPosition;" +
+                    " TexCoordOut = TexCoordIn;" +
+                    "}";
+
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "uniform sampler2D TexCoordIn;" +
+                    "uniform float posX;" +
+                    "uniform float posY;" +
+                    "varying vec2 TexCoordOut;" +
+                    "void main() {" +
+                    " gl_FragColor = texture2D(TexCoordIn, vec2,(TexCoordOut.x + posX, TexCoordOut.y + posY));"+
+                    "}";
+
+    private float texture[] = {
+            0f, 0.25f,
+            0f, 0f,
+            0.25f, 0f,
+            0.25f, 0.25f,
+    };
+
+    private int[] textures = new int[1];
+
+    //TODO: create constructor to initialize the variables below
+//    private final FloatBuffer vertexBuffer;
+//    private final FloatBuffer textureBuffer;
+//    private final ShortBuffer drawListBuffer;
+//    private final int mProgram;
+    private int mPositionHandle;
+    private int mColorHandle;
+    private int mMVPMatrixHandle;
+
+    static final int COORDS_PER_VERTEX = 3;
+    static final int COORDS_PER_TEXTURE = 2;
+    static final int textureStride = COORDS_PER_VERTEX * 4;
+    private final int vertexStride = COORDS_PER_VERTEX * 4;
+
+    public void loadTexture(int texture, Context context) {
+        InputStream imagestream = context.getResources().openRawResource(texture);
+        Bitmap bitmap = null;
+
+        android.graphics.Matrix flip = new android.graphics.Matrix();
+        flip.postScale(-1f, -1f);
+
+        try {
+            bitmap = BitmapFactory.decodeStream(imagestream);
+        } catch (Exception e) {
+            Log.d(TAG, "loadTexture: failed to load");
+        } finally {
+            try {
+                imagestream = null;
+                imagestream.close();
+            } catch (IOException e) {
+                Log.d(TAG, "loadTexture: failed to close imageStream");
+            }
+        }
+
+        GLES20.glGenTextures(1, textures, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0]);
+
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+        bitmap.recycle();
+    }
+
 }
