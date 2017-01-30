828819a594787a7451339b8ae875d8af60d030ea
elijahchen <code.elijahchen@gmail.com>
2017-01-29 23:19:07 -0500

    - Refactored GameRenderer class to implement Hero class

--- OpenGL_FunTime2/app/src/main/java/com/elijahcodes/opengl_funtime/GameRenderer.java
+++ OpenGL_FunTime2/app/src/main/java/com/elijahcodes/opengl_funtime/GameRenderer.java
@@ -21,10 +21,13 @@ public class GameRenderer implements GLSurfaceView.Renderer {
     public static float[] mMVPMatrix = new float[16];
     public static float[] mProjectionMatrix = new float[16];
     public static float[] mViewMatrix = new float[16];
+    public static float[] mTranslationMatrix = new float[16];
 
     private StarField starfield;
+    private Hero hero;
 
     float starfieldScroll = 0;
+    float heroSprite = 0;
 
     public GameRenderer(Context gameContext) {
         context = gameContext;
@@ -35,8 +38,11 @@ public class GameRenderer implements GLSurfaceView.Renderer {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         starfield = new StarField();
+        hero = new Hero();
 
         starfield.loadTexture(R.drawable.starfield_img, context);
+        //TODO: Add texture to drawables
+//        hero.loadTexture(R.drawable.spaceship_img, context);
     }
 
     @Override
@@ -55,10 +61,20 @@ public class GameRenderer implements GLSurfaceView.Renderer {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
         Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
-
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
         starfield.draw(mMVPMatrix, starfieldScroll);
+
+        GLES20.glEnable(GLES20.GL_BLEND);
+        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+
+        Matrix.setIdentityM(mTranslationMatrix,0);
+        Matrix.translateM(mTranslationMatrix, 0,0,-.5f,0);
+        Matrix.multiplyMM(matrix, 0, mMVPMatrix, 0, mTranslationMatrix, 0);
+
+        hero.draw(matrix,0,0);
+
+        GLES20.glDisable(GLES20.GL_BLEND);
 
         if(starfieldScroll == Float.MAX_VALUE){
             starfieldScroll = 0;
@@ -69,6 +85,7 @@ public class GameRenderer implements GLSurfaceView.Renderer {
 
     public static int loadShader(int type, String shaderCode){
         int shader = GLES20.glCreateShader(-type);
+
         GLES20.glShaderSource(shader, shaderCode);
         GLES20.glCompileShader(shader);
 
