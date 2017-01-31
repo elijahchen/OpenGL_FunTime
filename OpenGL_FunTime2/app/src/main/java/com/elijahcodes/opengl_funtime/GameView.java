3a1dbc959c87e40164c1662889be3856cf10f9d6
elijahchen <code.elijahchen@gmail.com>
2017-01-30 23:59:38 -0500

    - Added onTouchEvent to GameView class

--- OpenGL_FunTime2/app/src/main/java/com/elijahcodes/opengl_funtime/GameView.java
+++ OpenGL_FunTime2/app/src/main/java/com/elijahcodes/opengl_funtime/GameView.java
@@ -1,7 +1,12 @@
 package com.elijahcodes.opengl_funtime;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
+
+import static android.R.attr.x;
+import static android.R.attr.y;
 
 /**
  * Created by Elijah on 1/17/2017.
@@ -18,5 +23,13 @@ public class GameView extends GLSurfaceView{
         gameRenderer = new GameRenderer(context);
 
         setRenderer(gameRenderer);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        Point size = new Point();
+//        activity.getWindowManager().getDefaultDisplay().getSize(size);
+        int width = size.x / 2;
+        int height = size.y / 3;
+        int playableArea = size.y - height;
     }
 }
